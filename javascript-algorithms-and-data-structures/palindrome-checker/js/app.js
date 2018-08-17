c0e799b91250c60adf0822deb07655a26c191fea
Nilesh Laxmi <nileshlaxmi4@gmail.com>
2018-08-17 10:38:29 +0530

    Removed bugs and corrected workflow

--- javascript-algorithms-and-data-structures/palindrome-checker/js/app.js
+++ javascript-algorithms-and-data-structures/palindrome-checker/js/app.js
@@ -1,5 +1,5 @@
-function palindrome(str) {
-    // var str = $("#ipText").val();
+function palindrome() {
+    var str = $("#ipText").val();
     str = str.toLowerCase();
     str = str.replace(/[\s_,.()|:-]/g, "");
     
@@ -7,13 +7,13 @@ function palindrome(str) {
     console.log(newStr);
     if(newStr === str){
         output = true;
-        console.log(true);
-        return true;
+        // console.log(true);
+        // return true;
     }
     else{
         output = false;
-        console.log(false);
-        return false;
+        // console.log(false);
+        // return false;
     }
     $("#opText").text(output);
 }
@@ -23,15 +23,15 @@ function clearFields(){
     document.getElementById('opText').innerHTML = "";
 }
 
-palindrome("eye");
-palindrome("_eye");
-palindrome("race car");
-palindrome("not a palindrome");
-palindrome("A man, a plan, a canal. Panama");
-palindrome("never odd or even");
-palindrome("nope");
-palindrome("almostomla");
-palindrome("My age is 0, 0 si ega ym.");
-palindrome("1 eye for of 1 eye.")
-palindrome("0_0 (: /-\ :) 0-0");
-palindrome("five|\_/|four");
+// palindrome("eye");
+// palindrome("_eye");
+// palindrome("race car");
+// palindrome("not a palindrome");
+// palindrome("A man, a plan, a canal. Panama");
+// palindrome("never odd or even");
+// palindrome("nope");
+// palindrome("almostomla");
+// palindrome("My age is 0, 0 si ega ym.");
+// palindrome("1 eye for of 1 eye.")
+// palindrome("0_0 (: /-\ :) 0-0");
+// palindrome("five|\_/|four");
