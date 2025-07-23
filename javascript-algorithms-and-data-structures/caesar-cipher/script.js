6ab377acb990b6a54bb45e08c8030a13bfd38811
Nilesh Laxmi <nileshlaxmi4@gmail.com>
2025-07-23 09:37:48 +0530

    removed jquery and updated design for caesar cipher

--- javascript-algorithms-and-data-structures/caesar-cipher/script.js
+++ javascript-algorithms-and-data-structures/caesar-cipher/script.js
@@ -1,22 +1,22 @@
 const cipher = () => {
-    var str = $("#ipText").val();
+    let str = document.getElementById('ipText')?.value;
     let alphabets = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
     let index, newIndex, ar = [], newAr = [];
 
     str = str.toUpperCase();
     ar = str.split("");
 
-    for(let i=0; i<ar.length; i++){
+    for (let i = 0; i < ar.length; i++) {
         index = alphabets.indexOf(ar[i]);
-        if(index === -1){
+        if (index === -1) {
             newAr.push(ar[i]);
         }
-        else{
-            if(index <= 12){
+        else {
+            if (index <= 12) {
                 newIndex = index + 13;
                 newAr.push(alphabets[newIndex]);
             }
-            else{
+            else {
                 let value = 25 - index;
                 newIndex = 12 - value;
                 newAr.push(alphabets[newIndex]);
@@ -24,12 +24,11 @@ const cipher = () => {
         }
     }
     let newStr = newAr.join("");
-    console.log(newStr);
 
-    $("#opText").text(newStr);
+    document.getElementById('opText').innerText = newStr;
 }
 
-function clearFields(){
+function clearFields() {
     document.getElementById('ipText').value = "";
-    document.getElementById('opText').innerHTML = "";
+    document.getElementById('opText').innerText = "";
 }
