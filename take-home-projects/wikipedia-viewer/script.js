597e1798f76dc12ddefd8ff5810c82deb6751b1f
Nilesh Laxmi <nileshlaxmi4@gmail.com>
2025-07-27 13:36:14 +0530

    FREEC updated wikipedia viewer

--- take-home-projects/wikipedia-viewer/script.js
+++ take-home-projects/wikipedia-viewer/script.js
@@ -1,37 +1,48 @@
-$(document).ready(function() {
-  $('#search-button').click(function() {
-    let input = $('#search-tag').val();
-    var urlString = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + input + "&format=json&callback=?";
+document.addEventListener("DOMContentLoaded", function () {
+  const searchButton = document.getElementById("search-button");
+  const searchInput = document.getElementById("search-tag");
+  const resultList = document.getElementById("result");
+  const spinner = document.getElementById("spinner");
 
-    let wikiTopic, wikiDesc, wikiLinks, wikiLinksJSON;
-    let len = 0;
+  searchButton.addEventListener("click", function () {
+    const query = searchInput.value.trim();
+    if (!query) return;
 
-    $.ajax({
-      type: "GET",
-      dataType: "json",
-      async: "false",
-      url: urlString,
-      success: function(result) {
-        wikiTopic = result[1];
-        wikiDesc = result[2];
-        wikiLinks = result[3];
+    const url = `https://en.wikipedia.org/w/api.php?action=opensearch&search=${query}&format=json&origin=*`;
 
-        if (wikiTopic.length == 0) {
-          $("ul").text("<li><h4>No data found</h4></li>");
-        }
-        else {
-          $("ul").html("");
-          for (let i = 0; i < wikiTopic.length; i++) {
-            $("ul").append("<li><a href="+wikiLinks[i]+" target=_blank><h4>"+wikiTopic[i]+"</h4></a>"+"<p>"+wikiDesc[i]+"</p></li>");
+    // Clear previous results and show spinner
+    resultList.innerHTML = "";
+    spinner.style.display = "block";
+
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        const [_, titles, descriptions, links] = data;
+        resultList.innerHTML = "";
+
+        if (titles.length === 0) {
+          resultList.innerHTML = "<li class='fade-in'><h4>No results found</h4></li>";
+        } else {
+          for (let i = 0; i < titles.length; i++) {
+            const li = document.createElement("li");
+            li.classList.add("fade-in");
+            li.innerHTML = `
+              <a href="${links[i]}" target="_blank">
+                <h4>${titles[i]}</h4>
+              </a>
+              <p>${descriptions[i]}</p>
+            `;
+            resultList.appendChild(li);
           }
         }
-      },
-
-      error: function(err){
-        alert("Exception found"+err);
-      }
-
-    });
-  $('#search-tag').val("");
+      })
+      .catch(err => {
+        alert("Error fetching data.");
+        console.error(err);
+      })
+      .finally(() => {
+        spinner.style.display = "none";
+        searchInput.value = "";
+      });
   });
 });
